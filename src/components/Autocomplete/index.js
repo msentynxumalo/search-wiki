f44ab4d235db3897b22915667e3bfebced924da6
Sandile Nxumalo <Sandile@b1sa.co.za>
2024-09-18 17:54:38 +0200

    autocomplete component

---
+++ src/components/Autocomplete/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactAutocomplete from "react-autocomplete";
+
+const Autocomplete = ({ articles, searchValue, onSearchChange }) => {
+  <ReactAutocomplete
+    items={articles}
+    renderInput={Input}
+    inputProps={{ placeholder: "enter search term" }}
+    getItemValue={(item) => item.label}
+    renderMenu={(children, value, style) => {
+      return articles && articles.length ? (
+        <div style={{ ...style }} className="input-suggestions">
+          {children}
+          <a href={`/search?query=${value}`} className="search-link">
+            See all results
+          </a>
+        </div>
+      ) : (
+        <></>
+      );
+    }}
+    renderItem={(item, highlighted) => (
+      <div
+        key={item.id}
+        style={{ backgroundColor: highlighted ? "#eee" : "transparent" }}
+      >
+        {item.label}
+      </div>
+    )}
+    value={searchValue}
+    onChange={onSearchChange}
+  />;
+};
+
+export default Autocomplete;
