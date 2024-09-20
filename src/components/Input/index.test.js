9a5cf74a715445aaed35b6a2595243aee093fbc3
Sandile Nxumalo <Sandile@b1sa.co.za>
2024-09-20 12:48:07 +0200

    add enzyme installation

--- src/components/Input/index.test.js
+++ src/components/Input/index.test.js
@@ -1,13 +1,32 @@
 import React from "react";
-import {render} from "@testing-library/react";
+import {shallow} from "enzyme";
 import Input from ".";
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+const render = (props) => shallow (<Input {...props}/>);
 
 describe("Input component", () => {
     let sut;
     let props;
 
-    it("should match snapshot", () => {
-        sut = render(<Input {...props} />)
-        expect(sut).toMatchSnapshot();
-    })
+    describe('without props', () => { 
+        beforeEach(() => {
+            props = {
+                placeholder: 'Input your data',
+                name: 'search'
+            }
+        })
+
+        it('should render the component correctly', () => {
+            sut = render(<Input {...props}/>);
+        });
+
+        it("should match snapshot", () => {
+            expect(sut).toMatchSnapshot();
+        })
+     });
+
 })
