678ae02d433b12f72bd88737d1d76b0d4d0b3629
Sandile Nxumalo <Sandile@b1sa.co.za>
2024-09-20 07:53:48 +0200

    basic input component unit test

---
+++ src/components/Input/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {render} from "@testing-library/react";
+import Input from ".";
+
+describe("Input component", () => {
+    let sut;
+    let props;
+
+    it("should match snapshot", () => {
+        sut = render(<Input {...props} />)
+        expect(sut).toMatchSnapshot();
+    })
+})
